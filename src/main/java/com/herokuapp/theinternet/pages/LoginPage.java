e8d704f01f69a9cc903dc60714baa4fe8892f0cb
volodymyr.riabokon <volodymyr.riabokon@walgreens.com>
2022-11-07 18:34:48 +0100

    changed CH version, and NegativeLogInTests.java

--- src/main/java/com/herokuapp/theinternet/pages/LoginPage.java
+++ src/main/java/com/herokuapp/theinternet/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package com.herokuapp.theinternet.pages;
 
+import java.time.Duration;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,6 +10,7 @@ public class LoginPage extends BasePageObject {
     private By usernameLocator = By.id("username");
     private By passwordLocator = By.id("password");
     private By loginButtonLocator = By.tagName("button");
+    private By errorMessageLocator = By.id("flash");
 
     public LoginPage(WebDriver driver, Logger log) {
         super(driver, log);
@@ -20,5 +22,20 @@ public class LoginPage extends BasePageObject {
         type(password, passwordLocator);
         click(loginButtonLocator);
         return new SecureAreaPage(driver, log);
+    }
+
+    public void negativeLogin(String username, String password) {
+        log.info("Executing Negative LogIn with username [" + username + "] and password [" + password + "]");
+        type(username, usernameLocator);
+        type(password, passwordLocator);
+        click(loginButtonLocator);
+    }
+
+    public void waitForErrorMessage() {
+        waitForVisibilityOf(errorMessageLocator, Duration.ofSeconds(20));
+    }
+
+    public String getErrorMessageText() {
+        return find(errorMessageLocator).getText();
     }
 }
--- src/test/java/com/herokuapp/theinternet/loginpagetest/NegativeLogInTests.java
+++ src/test/java/com/herokuapp/theinternet/loginpagetest/NegativeLogInTests.java
@@ -1,8 +1,8 @@
 package com.herokuapp.theinternet.loginpagetest;
 
 import com.herokuapp.theinternet.base.TestUtilities;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import com.herokuapp.theinternet.pages.LoginPage;
+import com.herokuapp.theinternet.pages.WelcomePageObject;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -10,24 +10,21 @@ import org.testng.annotations.Test;
 public class NegativeLogInTests extends TestUtilities {
 
     @Parameters({"username", "password", "expectedMessage"})
-    @Test(priority = 2, groups = {"negativeTests", "smokeTests"})
-    public void negativeLoginTest(String username, String password, String expectedMessage) {
-       log.info("Starting negativeLoginTest with" + username + " and " + password);
-        String url = "https://the-internet.herokuapp.com/login";
-        driver.get(url);
-       log.info("Page is opened.");
-        WebElement usernameElement = driver.findElement(By.id("username"));
-        usernameElement.sendKeys(username);
-        WebElement passwordElement = driver.findElement(By.id("password"));
-        passwordElement.sendKeys(password);
-        WebElement logInButton = driver.findElement(By.tagName("button"));
-        logInButton.click();
-        WebElement unsuccessfulLogInMessage = driver.findElement(By.cssSelector("div#flash"));
-        String actualMessage = unsuccessfulLogInMessage.getText();
-        Assert.assertTrue(unsuccessfulLogInMessage.isDisplayed(), "Unsuccessful popUp is not visible");
-        Assert.assertTrue(actualMessage.contains(expectedMessage),
-                "Actual message does not contain expected message.\nActualMessage: "
-                        + actualMessage + "\nExpectedMessage: " + expectedMessage);
+    @Test(priority = 1, groups = {"negativeTests", "smokeTests"})
+    public void negativeLoginTest(String username, String password, String expectedErrorMessage) {
+        log.info("Starting negativeLoginTest with" + username + " and " + password);
+        //open main page
+        WelcomePageObject welcomePage = new WelcomePageObject(driver, log);
+        welcomePage.openPage();
+        //click on form authentication link
+        LoginPage loginPage = welcomePage.clickFormAuthenticationLink();
+        //execute negative login
+        loginPage.negativeLogin(username, password);
+        //wait for error msg
+        loginPage.waitForErrorMessage();
+        String message = loginPage.getErrorMessageText();
+        //verification
+        Assert.assertTrue(message.contains(expectedErrorMessage), "Message doesn't contain expected text.");
     }
 }
 
